1c67417eeab5f37b77622c8367cfde3b8d24464b
rafawhitee <rafaelnunes.inf@gmail.com>
2023-09-12 20:59:17 -0300

    bayes

--- machine-learning/curso-alura-02/bayes.py
+++ machine-learning/curso-alura-02/bayes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import BernoulliNB
 
 dados = pd.read_csv('clientes.csv')
 
@@ -33,25 +33,17 @@ norm = StandardScaler()
 X_normalizado = norm.fit_transform(X)
 X_Maria_normalizado = norm.transform(pd.DataFrame(X_Maria, columns=X.columns))
 
-# Distância Euclidiana de forma manual (entre a Maria e o primeiro cliente no X_normalizado)
-distancia_maria_e_cliente_0_normalizado = np.sqrt(
-    np.sum(np.square(X_Maria_normalizado - X_normalizado[0])))
-print(
-    f"distancia_maria_e_cliente_0_normalizado (manual): {distancia_maria_e_cliente_0_normalizado}")
-
-#####
-# Utilizando a lib para calcular e treinar já com a distância euclidiana e prever
-#####
-
+# faz a separação entre Treino e Teste
 X_treino, X_teste, Y_treino, Y_teste = train_test_split(
     X_normalizado, Y, test_size=0.3, random_state=123)
 
-knn = KNeighborsClassifier(metric='euclidean')
+# model
+model = BernoulliNB(binarize=0.44)
 
-knn.fit(X_treino, Y_treino)
+# treina
+model.fit(X_treino, Y_treino)
 
-predict_knn = knn.predict(X_teste)
-print(f"predict_knn: {predict_knn}")
+# testa
+predict_BNb = model.predict(X_teste)
 
-predict_maria_knn = knn.predict(X_Maria)
-print(f"predict_maria_knn: {predict_maria_knn}")
+print(predict_BNb)
