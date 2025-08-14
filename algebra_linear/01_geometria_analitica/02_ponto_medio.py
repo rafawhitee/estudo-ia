4f50b42b7a100effdc386ad93a35ef0a139a19d8
Rafael Nunes <rafaelnunes.inf@gmail.com>
2025-08-14 01:05:28 -0300

    Update 02_ponto_medio.py

--- algebra_linear/01_geometria_analitica/02_ponto_medio.py
+++ algebra_linear/01_geometria_analitica/02_ponto_medio.py
@@ -16,8 +16,11 @@ A = (0, 0)
 B = (3, 4)
 print(f"Ponto médio entre A{A} e B{B}: {ponto_medio_2d(A, B)}")
 
+def ponto_medio_3d(p1, p2):
+    # p1 e p2 são tuplas (x, y, z)
+    return ((p1[0] + p2[0])/2, (p1[1] + p2[1])/2, (p1[2] + p2[2])/2)
+
 # Exemplo 3: ponto médio entre pontos no espaço 3D
 x1, y1, z1 = 1, 2, 3
 x2, y2, z2 = 4, 6, 9
-ponto_medio_3d = ((x1 + x2)/2, (y1 + y2)/2, (z1 + z2)/2)
-print(f"Ponto médio 3D entre ({x1},{y1},{z1}) e ({x2},{y2},{z2}): {ponto_medio_3d}")
+print(f"Ponto médio 3D entre ({x1},{y1},{z1}) e ({x2},{y2},{z2}): {ponto_medio_3d((x1, y1, z1), (x2, y2, z2))}")
